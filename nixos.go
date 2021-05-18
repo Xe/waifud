555d35e340317f5e949098674c0f4ffeebb68424
Christine Dodrill <me@christine.website>
2021-05-17 20:54:05 -0400

    openstack?
    Signed-off-by: Christine Dodrill <me@christine.website>

--- nixos.go
+++ nixos.go
@@ -4,7 +4,7 @@ import "path/filepath"
 
 func mkNixOSImage(configFname, cacheDir, vmID string) (string, error) {
 	outputFname := filepath.Join(cacheDir, "nixos", vmID+".qcow2")
-	err := run("nix-shell", "-p", "nixos-generators", "--run", "nixos-generate -f qcow -o "+outputFname+" -c "+configFname)
+	err := run("nix-shell", "-p", "nixos-generators", "--run", "nixos-generate -f openstack -o "+outputFname+" -c "+configFname)
 	if err != nil {
 		return "", err
 	}
