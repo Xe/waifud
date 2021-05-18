89e4f8af714afa89b3dceaf8057b51fabf50251e
Christine Dodrill <me@christine.website>
2021-05-17 21:11:01 -0400

    oops lol
    Signed-off-by: Christine Dodrill <me@christine.website>

--- nixos.go
+++ nixos.go
@@ -3,7 +3,7 @@ package main
 import "path/filepath"
 
 func mkNixOSImage(configFname, cacheDir, vmID string) (string, error) {
-	outputFname := filepath.Join(cacheDir, "nixos", vmID+".qcow2")
+	outputFname := filepath.Join(cacheDir, "nixos", vmID)
 	err := run("nix-shell", "-p", "nixos-generators", "--run", "nixos-generate -f openstack -o "+outputFname+" -c "+configFname)
 	if err != nil {
 		return "", err
