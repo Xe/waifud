4b03ce40786cae886b35de769927698cb4ca552c
Christine Dodrill <me@christine.website>
2021-05-17 20:50:23 -0400

    oops
    Signed-off-by: Christine Dodrill <me@christine.website>

--- nixos.go
+++ nixos.go
@@ -4,7 +4,7 @@ import "path/filepath"
 
 func mkNixOSImage(configFname, cacheDir, vmID string) (string, error) {
 	outputFname := filepath.Join(cacheDir, "nixos", vmID+".qcow2")
-	err := run("nix-shell", "-p", "nixos-generators", "--run", "nixos-generate -f qcow -o "+outputFname)
+	err := run("nix-shell", "-p", "nixos-generators", "--run", "nixos-generate -f qcow -o "+outputFname+" -c "+configFname)
 	if err != nil {
 		return "", err
 	}
