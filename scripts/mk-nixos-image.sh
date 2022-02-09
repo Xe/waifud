85a95fb78632dcbc408432e6401e89dece68ef2d
Xe <me@christine.website>
2022-02-08 21:18:48 -0500

    scripts: create initial nixos image test
    Signed-off-by: Xe <me@christine.website>

--- scripts/mk-nixos-image.sh
+++ scripts/mk-nixos-image.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p nixos-generators
+#! nix-shell -i bash -p nixos-generators -p qemu
 
 set -ex
 
-NIX_PATH=nixpkgs=channel:nixos-unstable nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-unstable-within-$(date +%Y-%m-%d-%H-%M).qcow2
-NIX_PATH=nixpkgs=channel:nixos-21.11 nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-21.11-within-$(date +%Y-%m-%d-%H-%M).qcow2
+DATE="$(date +%Y-%m-%d-%H-%M)"
+
+NIX_PATH=nixpkgs=channel:nixos-unstable-small nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-unstable-within-${DATE}
+NIX_PATH=nixpkgs=channel:nixos-21.11-small nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-21.11-within-${DATE}
+
+qemu-img convert -c -O qcow2 ./nixos-unstable-within-${DATE}/nixos.qcow2 nixos-unstable-within-${DATE}.qcow2
+qemu-img convert -c -O qcow2 ./nixos-21.11-within-${DATE}/nixos.qcow2 nixos-21.11-within-${DATE}.qcow2
+
+sha256sum nixos-unstable-within-${DATE}.qcow2 > nixos-unstable-within-${DATE}.qcow2.sha256
+sha256sum nixos-21.11-within-${DATE}.qcow2 > nixos-21.11-within-${DATE}.qcow2.sha256
+
+rsync -avz --progress *.qcow2* lufta:/srv/http/xena.greedo.xeserv.us/pkg/nixos/
+
+rm ./nixos-unstable-within-${DATE} ./nixos-21.11-within-${DATE}
