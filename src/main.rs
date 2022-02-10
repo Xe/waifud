b4d930c261fe7a1ee90c2065813331c0d5caad94
Xe <me@christine.website>
2022-02-10 02:26:28 +0000

    make routes pretty
    Signed-off-by: Xe <me@christine.website>

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ use axum::{
 use std::sync::Arc;
 use tower::limit::ConcurrencyLimitLayer;
 use tower_http::trace::TraceLayer;
-use waifud::{Config, Result, State};
+use waifud::{
+    api::{self, cloudinit, distros, instances},
+    Config, Result, State,
+};
 
 #[tokio::main]
 async fn main() -> Result {
@@ -25,60 +28,30 @@ async fn main() -> Result {
         .layer(AddExtensionLayer::new(Arc::new(cfg)));
 
     let app = Router::new()
-        .route("/api/v1/distros", get(waifud::api::distros::list))
-        .route("/api/v1/distros", post(waifud::api::distros::create))
-        .route("/api/v1/distros/:name", post(waifud::api::distros::update))
-        .route("/api/v1/distros/:name", get(waifud::api::distros::get))
-        .route(
-            "/api/v1/distros/:name",
-            delete(waifud::api::distros::delete),
-        )
-        .route("/api/v1/instances", post(waifud::api::instances::create))
-        .route("/api/v1/instances", get(waifud::api::instances::list))
-        .route("/api/v1/instances/:id", get(waifud::api::instances::get))
-        .route(
-            "/api/v1/instances/:id/reboot",
-            post(waifud::api::instances::reboot),
-        )
-        .route(
-            "/api/v1/instances/:id/start",
-            post(waifud::api::instances::start),
-        )
-        .route(
-            "/api/v1/instances/:id/shutdown",
-            post(waifud::api::instances::shutdown),
-        )
+        .route("/api/v1/distros", get(distros::list))
+        .route("/api/v1/distros", post(distros::create))
+        .route("/api/v1/distros/:name", post(distros::update))
+        .route("/api/v1/distros/:name", get(distros::get))
+        .route("/api/v1/distros/:name", delete(distros::delete))
+        .route("/api/v1/instances", post(instances::create))
+        .route("/api/v1/instances", get(instances::list))
+        .route("/api/v1/instances/:id", get(instances::get))
         .route(
             "/api/v1/instances/:id/hardreboot",
-            post(waifud::api::instances::hard_reboot),
+            post(instances::hard_reboot),
         )
-        .route(
-            "/api/v1/instances/name/:name",
-            get(waifud::api::instances::get_by_name),
-        )
-        .route(
-            "/api/v1/instances/:id",
-            delete(waifud::api::instances::delete),
-        )
-        .route(
-            "/api/v1/instances/:id/machine",
-            get(waifud::api::instances::get_machine),
-        )
-        .route(
-            "/api/v1/libvirt/machines",
-            get(waifud::api::libvirt::get_machines),
-        )
-        .route(
-            "/api/cloudinit/:id/meta-data",
-            get(waifud::api::cloudinit::meta_data),
-        )
-        .route(
-            "/api/cloudinit/:id/user-data",
-            get(waifud::api::cloudinit::user_data),
-        )
+        .route("/api/v1/instances/:id/reboot", post(instances::reboot))
+        .route("/api/v1/instances/:id/start", post(instances::start))
+        .route("/api/v1/instances/:id/shutdown", post(instances::shutdown))
+        .route("/api/v1/instances/name/:name", get(instances::get_by_name))
+        .route("/api/v1/instances/:id", delete(instances::delete))
+        .route("/api/v1/instances/:id/machine", get(instances::get_machine))
+        .route("/api/v1/libvirt/machines", get(api::libvirt::get_machines))
+        .route("/api/cloudinit/:id/meta-data", get(cloudinit::meta_data))
+        .route("/api/cloudinit/:id/user-data", get(cloudinit::user_data))
         .route(
             "/api/cloudinit/:id/vendor-data",
-            get(waifud::api::cloudinit::vendor_data),
+            get(cloudinit::vendor_data),
         )
         .layer(middleware);
 
