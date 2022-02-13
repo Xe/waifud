0d2e88f5494a210544bd8bc18ebf06eb0a58da5d
Xe <me@christine.website>
2022-02-13 19:28:45 +0000

    waifuctl only package
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -12,17 +12,29 @@
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in {
+        packages = {
+          waifud-full = naersk-lib.buildPackage {
+            src = ./.;
+            buildInputs = with pkgs; [
+              pkg-config
+              openssl
+              sqliteInteractive
+              libvirt
+            ];
+          };
 
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-          buildInputs = with pkgs; [
-            pkg-config
-            openssl
-            sqliteInteractive
-            libvirt
-          ];
+          waifuctl = pkgs.stdenv.mkDerivation {
+            pname = "waifuctl";
+            version = self.packages."${system}".waifud-full.version;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ${self.packages."${system}".waifud-full}/bin/waifuctl $out/bin
+            '';
+          };
         };
 
+        defaultPackage = self.packages.waifud-full;
         defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
 
         devShell = with pkgs;
