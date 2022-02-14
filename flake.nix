f66f0954f99a30d350e3e1cd0056002a6729e1dc
Xe <me@christine.website>
2022-02-14 02:13:14 +0000

    start on nixos configuration for waifud
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           };
         };
 
-        defaultPackage = self.packages."${system}".waifud;
+        defaultPackage = self.packages."${system}".waifuctl;
 
         apps = {
           waifud = utils.lib.mkApp { drv = self.packages."${system}".waifud; };
@@ -43,7 +43,131 @@
             utils.lib.mkApp { drv = self.packages."${system}".waifuctl; };
         };
 
-        defaultApp = self.apps."${system}".waifud;
+        defaultApp = self.apps."${system}".waifuctl;
+
+        nixosModules = {
+          waifuctl = { ... }: {
+            environment.systemPackages = [ self.packages."${system}".waifuctl ];
+          };
+
+          waifud-common = { lib, ... }: {
+            users.groups.waifud = lib.mkDefault { };
+          };
+
+          waifud-host = { lib, pkgs, config, ... }:
+            with lib;
+            let
+              cfg = config.xeserv.waifud;
+              cfgJSON = pkgs.writeTextFile {
+                name = "waifud.json";
+                text = toJSON cfg;
+              };
+              cfgDhall = pkgs.stdenv.mkDerivation {
+                name = "waifud-config-latest";
+                src = cfgJSON;
+                inputs = [ pkgs.dhall-json ];
+                phases = "installPhase";
+                installPhase = ''
+                  cat $src | json-to-dhall --output $out
+                '';
+              };
+            in {
+              imports = [
+                self.nixosModules."${system}".waifud-common
+                self.nixosModules."${system}".waifuctl
+              ];
+
+              options = {
+                baseURL = mkOption {
+                  type = types.str;
+                  default = "http://192.168.122.1:23818";
+                  description =
+                    "the base URL for VMs to poke waifud for config";
+                };
+
+                hosts = mkOption {
+                  type = with types; listOf str;
+                  default = [ "kos-mos" "logos" "ontos" "pneuma" ];
+                  description =
+                    "the list of hosts that waifud can manage (ip address/domain name)";
+                };
+
+                bindHost = mkOption {
+                  type = types.str;
+                  default = "::";
+                  description = "the host to bind waifud on";
+                };
+
+                port = mkOption {
+                  type = types.port;
+                  default = 23818;
+                  description = "the port that waifud should bind on";
+                };
+              };
+
+              config = {
+                users.users.waifud = {
+                  createHome = true;
+                  description = "waifud user";
+                  isSystemUser = true;
+                  group = "waifud";
+                  home = "/var/lib/waifud";
+                };
+
+                systemd.services = {
+                  waifud-ssh-agent = {
+                    wantedBy = [ "multi-user.target" ];
+                    serviceConfig = {
+                      User = "waifud";
+                      Group = "waifud";
+                      Restart = "always";
+                      WorkingDirectory = "/var/lib/waifud";
+                      ExecStart =
+                        "${pkgs.openssh}/bin/ssh-agent -D -a /var/lib/waifud/agent.sock";
+                    };
+                  };
+
+                  waifud = {
+                    wantedBy = [ "multi-user.target" ];
+
+                    environment = {
+                      RUST_LOG = "tower_http=debug,waifud=debug,info";
+                      SSH_AUTH_SOCK = "/var/lib/waifud/agent.sock";
+                    };
+                    serviceConfig = {
+                      User = "waifud";
+                      Group = "waifud";
+                      Restart = "always";
+                      WorkingDirectory = "/var/lib/waifud";
+                      RestartSec = "30s";
+                      ExecStartPre = [
+                        "rm -f ./config.dhall"
+                        "cp ${cfgDhall} ./config.dhall"
+                      ];
+                      ExecStart =
+                        "${self.packages."${system}".waifud}/bin/waifud";
+                    };
+                  };
+                };
+              };
+            };
+
+          waifud-runner = { pkgs, lib, ... }: {
+            imports = [ self.nixosModules."${system}".waifud-common ];
+
+            environment.systemPackages = with pkgs; [ qemu zfs wget ];
+            services.libvirt.enable = lib.mkDefault true;
+
+            security.polkit.extraConfig = ''
+              /* Allow users in the waifud group to manage the libvirt daemon without authentication */
+              polkit.addRule(function(action, subject) {
+                  if (action.id == "org.libvirt.unix.manage" && subject.isInGroup("waifud")) {
+                          return polkit.Result.YES;
+                  }
+              });
+            '';
+          };
+        };
 
         devShell = with pkgs;
           mkShell {
@@ -62,7 +186,6 @@
               go
               goimports
               gopls
-              cdrkit
               jq
               jo
             ];
