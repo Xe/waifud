e0ed0192826c59ed8a1a76e9ba5a78b78144095c
Xe <me@christine.website>
2022-08-15 00:16:03 +0000

    fix tests and add rocky linux scraping
    Signed-off-by: Xe <me@christine.website>

--- src/scrape/mod.rs
+++ src/scrape/mod.rs
@@ -6,18 +6,23 @@ use rusqlite::params;
 use tokio::time::Instant;
 
 pub mod arch;
+pub mod rocky_linux;
 pub mod ubuntu;
 
 pub async fn get_all() -> Result<Vec<Distro>> {
-    let distro_scrapes = join_all(vec![
+    let ubuntus = join_all(vec![
         ubuntu::scrape(("22.04", "jammy")),
         ubuntu::scrape(("20.04", "focal")),
         ubuntu::scrape(("18.04", "bionic")),
     ])
     .await;
+    let rockies = join_all(vec![rocky_linux::scrape(8), rocky_linux::scrape(9)]).await;
     let mut result: Vec<Distro> = vec![arch::scrape().await?];
 
-    for distro in distro_scrapes {
+    for distro in ubuntus {
+        result.push(distro?);
+    }
+    for distro in rockies {
         result.push(distro?);
     }
 
---
+++ src/scrape/rocky_linux.rs
@@ -0,0 +1,114 @@
+use scraper::{ElementRef, Html};
+use std::collections::HashMap;
+
+use crate::{models::Distro, Error};
+
+/// # Scraper for Rocky Linux cloud images
+///
+/// This scrapes the Rocky Linux cloud image site and extracts out the URL of the latest
+/// release of Rocky Linux and its sha256 sum.
+
+const RELEASE_BASE: &'static str = "http://download.rockylinux.org/pub/rocky/";
+
+#[instrument]
+pub async fn scrape(version: i32) -> crate::Result<crate::models::Distro> {
+    let sel = scraper::Selector::parse("a").expect("selector to parse");
+
+    let mut base: String = RELEASE_BASE.to_string();
+    base.push_str(&format!("{}", version));
+    base.push_str("/images/");
+
+    if version == 9 {
+        base.push_str("x86_64/");
+    }
+
+    let u = url::Url::parse(&base)?;
+    debug!("url: {u}");
+
+    let response_html = reqwest::get(u.as_str())
+        .await?
+        .error_for_status()?
+        .text()
+        .await?;
+
+    let doc = Html::parse_document(&response_html);
+
+    let elems = doc.select(&sel).collect::<Vec<ElementRef>>();
+    let elems = elems
+        .into_iter()
+        .rev()
+        .filter(|elem| elem.value().attr("href").is_some())
+        .map(|elem| elem.value().attr("href").unwrap())
+        .filter(|link| link.contains("x86_64"))
+        .filter(|link| !link.contains("latest"))
+        .filter(|link| link.ends_with(".qcow2"))
+        .collect::<Vec<&str>>();
+    let link = elems.get(0).ok_or(crate::Error::Catchall(
+        "can't get second to last element of image list".to_string(),
+    ))?;
+
+    let u = u.join(link)?;
+    debug!(url = u.to_string(), link = link);
+
+    let image_url = u.to_string();
+    let shasum_url = u.join("./CHECKSUM")?;
+
+    debug!("shasum url: {shasum_url}");
+    let mut shasums = reqwest::get(shasum_url)
+        .await?
+        .error_for_status()?
+        .text()
+        .await?;
+    if version != 8 {
+        shasums = shasums
+            .split("\n")
+            .filter(|line| line.contains("SHA256"))
+            .collect::<Vec<&str>>()
+            .join("\n");
+    }
+
+    let mut shasum = String::new();
+
+    for line in shasums.split("\n").filter(|line| line.contains(link)) {
+        if line == "" {
+            break;
+        }
+        if version != 8 {
+            let sides: Vec<&str> = line.split(" ").collect();
+            if sides.len() != 4 {
+                error!("Somehow this doesn't have 3 spaces in it {line:?}");
+                continue;
+            }
+            shasum = sides.get(3).unwrap().to_string()
+        } else {
+            let sides: Vec<&str> = line.split("  ").collect();
+            if sides.len() != 2 {
+                error!("Somehow this doesn't have two spaces in it {line:?}");
+                continue;
+            }
+            shasum = sides.get(0).unwrap().to_string();
+        }
+    }
+
+    let image_url = u.join(&image_url)?.as_str().to_string();
+
+    Ok(Distro {
+        name: format!("rocky-linux-{version}"),
+        download_url: image_url,
+        sha256sum: shasum.to_string(),
+        min_size: 10,
+        format: "waifud://qcow2".to_string(),
+    })
+}
+
+fn shuck(value: &str) -> &str {
+    let mut chars = value.chars();
+    chars.next();
+    chars.next_back();
+    chars.as_str()
+}
+
+#[test]
+fn test_shuck() {
+    assert_eq!(shuck("\"hi there\""), "hi there");
+}
