08e6946b8d0d6d0f7198cd5971713e5ed8c1d607
Xe <me@christine.website>
2022-08-17 19:24:35 +0000

    update nixos image generation
    Signed-off-by: Xe <me@christine.website>

--- scripts/mk-nixos-image.sh
+++ scripts/mk-nixos-image.sh
@@ -6,14 +6,14 @@ set -ex
 DATE="$(date +%Y-%m-%d-%H-%M)"
 
 NIX_PATH=nixpkgs=channel:nixos-unstable-small nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-unstable-within-${DATE}
-NIX_PATH=nixpkgs=channel:nixos-21.11-small nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-21.11-within-${DATE}
+NIX_PATH=nixpkgs=channel:nixos-22.05-small nixos-generate -f qcow -c ./nixos-image.nix -o ./nixos-22.05-within-${DATE}
 
 qemu-img convert -c -O qcow2 ./nixos-unstable-within-${DATE}/nixos.qcow2 nixos-unstable-within-${DATE}.qcow2
-qemu-img convert -c -O qcow2 ./nixos-21.11-within-${DATE}/nixos.qcow2 nixos-21.11-within-${DATE}.qcow2
+qemu-img convert -c -O qcow2 ./nixos-22.05-within-${DATE}/nixos.qcow2 nixos-22.05-within-${DATE}.qcow2
 
 sha256sum nixos-unstable-within-${DATE}.qcow2 > nixos-unstable-within-${DATE}.qcow2.sha256
-sha256sum nixos-21.11-within-${DATE}.qcow2 > nixos-21.11-within-${DATE}.qcow2.sha256
+sha256sum nixos-22.05-within-${DATE}.qcow2 > nixos-22.05-within-${DATE}.qcow2.sha256
 
 rsync -avz --progress *.qcow2* lufta:/srv/http/xena.greedo.xeserv.us/pkg/nixos/
 
-rm ./nixos-unstable-within-${DATE} ./nixos-21.11-within-${DATE}
+rm ./nixos-unstable-within-${DATE} ./nixos-22.05-within-${DATE}
